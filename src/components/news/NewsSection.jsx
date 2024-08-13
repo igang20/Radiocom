7ca697a02aa748e4c7a1ace79659899d7e7367e5
Ilya <65218566+igang20@users.noreply.github.com>
2024-08-13 12:46:31 +0300

    добавлена новая зависимость, исправлены картинки

--- src/components/news/NewsSection.jsx
+++ src/components/news/NewsSection.jsx
@@ -4,6 +4,7 @@ export default function NewsSection() {
     return (
         <section className='news-section'>
             <div className='news-content'>
+                <img src="/imgs/BcgReferences/Newspaper.svg" alt="Newspaper" className='bcg-img2' />
                 <h1 className='news-heading'>Новости компании</h1>
                 <div className='cards-section'>
                     <NewsCard /><NewsCard />
