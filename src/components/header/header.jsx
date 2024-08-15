1f3bc6764e45908ea33a4e45275620ddb3738b6f
Ilya <65218566+igang20@users.noreply.github.com>
2024-08-15 13:33:06 +0300

    ьщишду руфвук

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,20 +2,34 @@
 import "./header.css";
 
 import { Link } from "react-router-dom";
+import useWindowDimensions from "../useWindowDimensions/WindowDimensions"
+
+import MobileHeader from "./mobileHeader/MobileHeader";
 
 export default function Header() {
+  const dimensions = useWindowDimensions()
 
+  console.log(dimensions.width)
+  console.log(window.innerWidth)
   return (
-    <header>
-      <div className="header-content">
-        <Link to='/'><img src="/imgs/logotype.svg" className="logo" alt='logo' /></Link>
-        <div className="header-links">
-          <Link to="/payment">Оплата</Link>
-          <Link to="/support">Поддержка</Link>
-          <Link to="#">Тест скорости</Link>
-          <Link to="/about">О компании</Link>
-        </div>
-      </div>
-    </header>
+    <>
+      {dimensions.width > 1024 ?
+        <header>
+          <div className="header-content">
+            <Link to='/'><img src="/imgs/logotype.svg" className="logo" alt='logo' /></Link>
+            <div className="header-links">
+              <Link to="/payment">Оплата</Link>
+              <Link to="/support">Поддержка</Link>
+              <Link to="#">Тест скорости</Link>
+              <Link to="/about">О компании</Link>
+            </div>
+          </div>
+        </header >
+        :
+        <MobileHeader />
+      }
+    </>
+
+
   );
 }
