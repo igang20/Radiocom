0a4bc503b827f49049332a1f067e06e8c914d6c5
max-maximovv <165264888+max-maximovv@users.noreply.github.com>
2024-08-15 13:38:19 +0300

    00011

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,37 +2,23 @@
 import "./header.css";
 
 import { Link } from "react-router-dom";
-import useWindowDimensions from "../useWindowDimensions/WindowDimensions"
+import useWindowDimensions from "../useWindowDimensions/WindowDimensions";
 
 import MobileHeader from "./mobileHeader/MobileHeader";
 
 export default function Header() {
-<<<<<<< HEAD
-  return (
-    <header>
-      <div className="header-content">
-        <Link to="/">
-          <img src="/imgs/logotype.svg" className="logo" alt="logo" />
-        </Link>
-        <div className="header-links">
-          <Link to="/payment">Оплата</Link>
-          <Link to="/support">Поддержка</Link>
-          <Link to="#">Тест скорости</Link>
-          <Link to="/about">О компании</Link>
-        </div>
-      </div>
-    </header>
-=======
-  const dimensions = useWindowDimensions()
+  const dimensions = useWindowDimensions();
 
-  console.log(dimensions.width)
-  console.log(window.innerWidth)
+  console.log(dimensions.width);
+  console.log(window.innerWidth);
   return (
     <>
-      {dimensions.width > 1024 ?
+      {dimensions.width > 1024 ? (
         <header>
           <div className="header-content">
-            <Link to='/'><img src="/imgs/logotype.svg" className="logo" alt='logo' /></Link>
+            <Link to="/">
+              <img src="/imgs/logotype.svg" className="logo" alt="logo" />
+            </Link>
             <div className="header-links">
               <Link to="/payment">Оплата</Link>
               <Link to="/support">Поддержка</Link>
@@ -40,13 +26,10 @@ export default function Header() {
               <Link to="/about">О компании</Link>
             </div>
           </div>
-        </header >
-        :
+        </header>
+      ) : (
         <MobileHeader />
-      }
+      )}
     </>
-
-
->>>>>>> 1f3bc6764e45908ea33a4e45275620ddb3738b6f
   );
 }
