2d7cf7e381d7948c295487f323a817a37184f672
max-maximovv <165264888+max-maximovv@users.noreply.github.com>
2024-08-16 12:37:29 +0300

    mobile rates swiper

--- src/components/main-page/news-section/newsSection.jsx
+++ src/components/main-page/news-section/newsSection.jsx
@@ -15,11 +15,11 @@ export default function NewsSection() {
   return (
     <section className="news-section">
       <div className="news-content">
-        <img
+        {/*<img
           src="/imgs/BcgReferences/Newspaper.svg"
           alt="Newspaper"
           className="bcg-img2"
-        />
+        />*/}
         <h1 className="news-heading">Новости компании</h1>
         <div>
           {data ? (
